cb5e82dd22037379f7c04b435e4707dd338e8534
cvsoli4 <cvsoli4>
2004-08-17 08:42:35 +0000

    cleaned the prototypes and headers a bit, should remove some compiler warnings

--- src/jk_lib.h
+++ src/jk_lib.h
@@ -1,4 +1,7 @@
+#ifndef __JK_LIB_H
+#define __JK_LIB_H
 
+#include "config.h"
 
 #ifdef DEBUG
 #define DEBUG_MSG printf
@@ -13,9 +16,12 @@
 #define DEBUG_LOG(args...)
  /**/
 #endif
- 
 
+#ifndef HAVE_MALLOC0
 #define malloc0(size) memset(malloc(size),0,size)
+#define HAVE_MALLOC0
+#endif /* HAVE_MALLOC0 */
+
 void testsafepath(const char *path, int owner, int group);
 int getjaildir(const char *oldhomedir, char **jaildir, char **newhomedir);
 char *strip_string(char * string);
@@ -23,3 +29,5 @@ int count_char(const char *string, char lookfor);
 char **explode_string(const char *string, char delimiter);
 int count_array(char **arr);
 void free_array(char **arr);
+
+#endif /* __JK_LIB_H */
--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,12 @@
+#ifndef __UTILS_H
+#define __UTILS_H
 
 #include "config.h"
+
+#ifndef HAVE_MALLOC0
+#define malloc0(size) memset(malloc(size),0,size)
+#define HAVE_MALLOC0
+#endif /* HAVE_MALLOC0 */
 
 #ifndef HAVE_STRNDUP
 char *strndup(const char *s, size_t n);
@@ -18,3 +25,13 @@ char *stpcpy(char *dest, const char *src);
 #endif /* HAVE_WORDEXP */
 
 char *return_malloced_getwd(void);
+
+#ifndef HAVE_CLEARENV
+int clearenv(void);
+#endif
+
+#ifndef HAVE_GETCURRENTDIRNAME
+char *get_current_dir_name(void);
+#endif
+
+#endif /* __UTILS_H */
